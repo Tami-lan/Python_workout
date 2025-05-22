4a0c70925ef20e2d081a24cb20f60c93dc6360f0
Tami-lan T <upgradestamila@gmail.com>
2025-05-22 15:08:48 +0000

    updated

--- Python_practice/Game.py
+++ Python_practice/Game.py
@@ -1,5 +1,5 @@
 import random
-rock ="""
+Rock ="""
     _______
 ---'   ____)
       (_____)
@@ -25,4 +25,22 @@ Scissor="""
       (____)
 ---.__(___)
 """
+li=[Rock,Paper,Scissor]
+user=int(input("Enter the choice as number rock-0 , paper-1, sciccor-2: "))
+computer =random.randint(0,2)
 
+if user>=3 or user<0:
+    print("Enter the Valid Choice Number ")
+else:
+    print("User Choice::::\n",li[user])
+    print("Computer Choice::::\n",li[computer])
+    if user==0 and computer==2:
+        print("You Won! Rock smashed the scissor")
+    elif computer==0 and user==2:
+        print("Your are the looser! u are smashed")
+    elif user>computer:
+        print("You Won!")
+    elif computer>user:
+        print("You are the looser")
+    elif user==computer:
+        print("All Tie")
