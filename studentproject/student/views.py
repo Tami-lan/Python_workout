879a50715d8adc6c537bac777805219fe55862dd
Tami-lan T <upgradestamila@gmail.com>
2025-06-02 07:05:55 +0000

    updated

--- studentproject/student/views.py
+++ studentproject/student/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(response):
     return HttpResponse("Hello, Django Beginner!")
+
+def list_student(response):
+    students=Student.objects.all()
+    return HttpResponse(students)
