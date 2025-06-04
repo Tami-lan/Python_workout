858de9f357245d6dd0f4a2c127348c7a2bde3cb0
Tami-lan T <upgradestamila@gmail.com>
2025-06-04 08:10:13 +0000

    updated

--- studentproject/student/models.py
+++ studentproject/student/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
+class Student(models.Model):
+    Name=models.CharField(max_length=100)
+    Course_Name=models.CharField(max_length=100)
+    Student_Id=models.IntegerField()
+    Email_address=models.EmailField(max_length=100,unique=True)
+    Contact_No=models.CharField(max_length=100,blank=True)
+    photo=models.ImageField(upload_to='images')
+    Created_at=models.DateTimeField(auto_now_add=True)
+    Updated_at=models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+           return self.Name
 
 
 
--- studentproject/studentproject/views.py
+++ studentproject/studentproject/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from student.models import Student
 
 def home(request):
+    student=Student.objects.all()
+    print(student)
     return render(request,'home.html')
