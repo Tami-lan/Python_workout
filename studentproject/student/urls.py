e62ce0ff8320fab671c43a51c8c0c016288b52de
Tami-lan T <upgradestamila@gmail.com>
2025-06-06 07:19:15 +0000

    updation

--- studentproject/student/urls.py
+++ studentproject/student/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('<int:pk>/',views.student_detail),
+    path('<int:pk>/',views.student_detail, name='student_detail'),
 ]
 
 
--- studentproject/studentproject/urls.py
+++ studentproject/studentproject/urls.py
@@ -25,7 +25,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
+    path('',views.home, name='home'),
     path('student/', include('student.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
