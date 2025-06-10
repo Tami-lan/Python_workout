c48ea16e5563c413d5c148cbf73a45ab4d4695c4
Tami-lan T <upgradestamila@gmail.com>
2025-06-10 15:18:54 +0000

    updated

--- ToDo/todo_main/todo_main/views.py
+++ ToDo/todo_main/todo_main/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("<h4>This is my Home page</h4>")
+    return render(request,'home.html')
